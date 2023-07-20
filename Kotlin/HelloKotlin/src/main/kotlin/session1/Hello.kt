d871b944eec5a3d1d31b2a0ba654d2e974ef9e29
amiratashani <amiratashanii@gmail.com>
2023-07-20 11:20:20 +0330

    add session 1

---
+++ Kotlin/HelloKotlin/src/main/kotlin/session1/Hello.kt
@@ -0,0 +1,4 @@
+package session1
+fun main() {
+    println("Hello World!")
+}
---
+++ Kotlin/HelloKotlin/src/main/kotlin/session1/Varaible.kt
@@ -0,0 +1,17 @@
+package session1
+const val MAX_EXPERIENCE : Int = 500
+
+fun main(){
+
+    var experiencePoints: Int  = 12
+    experiencePoints += 45
+
+    val playerName = "Amir"
+
+    val isGameStart = true
+
+    println(isGameStart)
+    println(experiencePoints)
+    println(playerName)
+    println(MAX_EXPERIENCE)
+}
