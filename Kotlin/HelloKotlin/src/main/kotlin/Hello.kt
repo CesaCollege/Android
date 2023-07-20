925af6b448231eeb1d4a65346de56fb28bd1a856
amiratashani <amiratashanii@gmail.com>
2023-07-20 11:06:14 +0330

    hello kotlin

---
+++ Kotlin/HelloKotlin/src/main/kotlin/Hello.kt
@@ -0,0 +1,15 @@
+const val MAX_EXPERIENCE : Int = 500
+
+fun main(){
+    var experiencePoints: Long  = 1200000002255
+    experiencePoints = 1
+    println(experiencePoints)
+
+    val playerName = "Amir"
+    println(playerName)
+
+    val startGame = true
+    println(startGame)
+
+    println(MAX_EXPERIENCE)
+}
